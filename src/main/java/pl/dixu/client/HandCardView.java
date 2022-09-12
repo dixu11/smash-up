dfe3499b30458b89bac0625ccbc365e5477082d4
Daniel Szlicht <daniel.szlicht25@gmail.com>
2022-09-12 18:40:39 +0200

    hand view in progress

---
+++ src/main/java/pl/dixu/client/HandCardView.java
@@ -0,0 +1,5 @@
+package pl.dixu.client;
+
+public class HandCardView {
+
+}
---
+++ src/main/java/pl/dixu/client/HandView.java
@@ -0,0 +1,21 @@
+package pl.dixu.client;
+
+import pl.dixu.server.card.Card;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandView {
+
+    private List<HandCardView> handCardViews = new ArrayList<>();
+
+    public void addCard(Card card){
+
+    }
+
+    public void render(Graphics graphics) {
+
+    }
+
+}
